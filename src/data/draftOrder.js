e6ee8acba246a21b5a960358d3eaf032e5ce49c5
zachstine <zach.stine@ymail.com>
2026-03-12 16:33:32 -0400

    fixed order for now

--- src/data/draftOrder.js
+++ src/data/draftOrder.js
@@ -1,22 +1,34 @@
-import { NFL_TEAMS } from './teams';
+import { draftCapital2026 } from './draftCapital2026';
 
-const baseOrder = NFL_TEAMS.map((team) => team.abbr);
+const ROUND_ENDS = [32, 64, 100, 136, 176, 219, 257];
+
+function getRoundFromOverall(overall) {
+  for (let i = 0; i < ROUND_ENDS.length; i += 1) {
+    if (overall <= ROUND_ENDS[i]) {
+      return i + 1;
+    }
+  }
+  return 7;
+}
+
+function getRoundStart(round) {
+  return round === 1 ? 1 : ROUND_ENDS[round - 2] + 1;
+}
 
 export function buildDraftBoard(totalRounds = 7) {
-  const board = [];
-  let overall = 1;
-
-  for (let round = 1; round <= totalRounds; round += 1) {
-    baseOrder.forEach((team, index) => {
-      board.push({
+  const allSlots = Object.entries(draftCapital2026)
+    .flatMap(([team, picks]) =>
+      picks.map((overall) => ({
         overall,
-        round,
-        pickInRound: index + 1,
+        round: getRoundFromOverall(overall),
         team,
-      });
-      overall += 1;
-    });
-  }
+      }))
+    )
+    .sort((a, b) => a.overall - b.overall)
+    .map((slot) => ({
+      ...slot,
+      pickInRound: slot.overall - getRoundStart(slot.round) + 1,
+    }));
 
-  return board;
+  return allSlots.filter((slot) => slot.round <= totalRounds);
 }
