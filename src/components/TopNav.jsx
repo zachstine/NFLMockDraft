c501eb473ebd9971ec39ff3f9c571d08fbfe6ce4
zachstine <zach.stine@ymail.com>
2026-03-13 13:36:41 -0400

    deleted weird button

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -69,16 +69,6 @@ export default function TopNav() {
           Groups
         </button>
 
-        {isGroupPage ? (
-          <button
-            type="button"
-            className="secondary-btn"
-            onClick={() => navigate(location.pathname)}
-          >
-            Group
-          </button>
-        ) : null}
-
         <button
           type="button"
           className="secondary-btn"
